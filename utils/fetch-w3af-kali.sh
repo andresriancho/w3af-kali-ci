340a539f7d8821cb55f4135f9e8a5bfe3847a482
Andres Riancho <andres.riancho@gmail.com>
2015-04-22 12:00:10 -0300

    Better repo fetch

--- utils/fetch-w3af-kali.sh
+++ utils/fetch-w3af-kali.sh
@@ -8,16 +8,22 @@ set -e
 #
 if [ ! -d "w3af-kali" ]; then
     git clone git@github.com:andresriancho/w3af-kali.git
+    git checkout --track origin/upstream
+    git checkout --track origin/pristine-tar
+    git remote add kali-upstream git://git.kali.org/packages/w3af.git
 fi
 
 cd w3af-kali
 
+git checkout upstream
 git pull
-git checkout --track origin/upstream
-git checkout --track origin/pristine-tar
-git checkout master
 
-git remote add kali-upstream git://git.kali.org/packages/w3af.git || true
+git checkout pristine-tar
+git pull
+
+git checkout master
+git pull
+
 git fetch -v kali-upstream
 
 git checkout master
